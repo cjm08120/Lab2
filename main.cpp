a2758b7f3945427969c2b89228980fdf271689a0
cjm <cjm08120@snu.ac.kr>
2024-06-07 16:19:19 +0900

    modify func

--- main.cpp
+++ main.cpp
@@ -3,16 +3,22 @@
 
 using namespace std;
 
-int N,M;
-vector<int> vec;
+static int N,M;
+static vector<int> vec;
 
-void func(int cnt) {
+void func(int last) {
 
+	if(vec.size()==M){
+      for(auto x : vec)cout << x << " ";
+      cout << '\n';
+      return;
+   }
 
-
-
-
-
+   for(int i=last+1;i<=N;i++){
+      vec.push_back(i);
+      func(i);
+      vec.pop_back();
+   }
 
 }
 
